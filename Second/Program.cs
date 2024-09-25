b2a199d8e787d5cc68b759e033de16d8f0c23fe2
slayd <slayd@HOME-PC>
2024-09-26 00:36:33 +0500

    Initial commit

---
+++ Second/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int readerCount = 10;
+            int writerCount = 3;
+            
+            Task[] readers = new Task[readerCount];
+            for (int i = 0; i < readerCount; i++)
+            {
+                readers[i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < 5; j++) 
+                    {
+                        int value = Server.GetCount();
+                        Console.WriteLine($"Читатель {Task.CurrentId}: значение count = {value}");
+                        Thread.Sleep(100); 
+                    }
+                });
+            }
+
+            // Задачи для писателей
+            Task[] writers = new Task[writerCount];
+            for (int i = 0; i < writerCount; i++)
+            {
+                writers[i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < 5; j++) 
+                    {
+                        int increment = new Random().Next(1, 10);
+                        Server.AddToCount(increment);
+                        Console.WriteLine($"Писатель {Task.CurrentId}: добавил {increment}, новое значение count = {Server.GetCount()}");
+                        Thread.Sleep(150); 
+                    }
+                });
+            }
+
+            // Ожидаем завершения всех задач
+            Task.WaitAll(readers);
+            Task.WaitAll(writers);
+
+            Console.WriteLine("Все задачи завершены");
+            Console.WriteLine($"Итоговое значение count = {Server.GetCount()}");
+
+        }
+        public static class Server {
+            private static int count = 0;
+            private static ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+
+            // Метод для чтения значения переменной count
+            public static int GetCount()
+            {
+                rwLock.EnterReadLock(); 
+                try
+                {
+                    return count;
+                }
+                finally
+                {
+                    rwLock.ExitReadLock(); 
+                }
+            }
+
+            // Метод для добавления значения к переменной count
+            public static void AddToCount(int value)
+            {
+                rwLock.EnterWriteLock(); 
+                try
+                {
+                    count += value;
+                }
+                finally
+                {
+                    rwLock.ExitWriteLock(); 
+                }
+            }
+        } 
+    }
+}
+
---
+++ Second/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Second")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Second")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("143e882d-7d53-4ba9-93e4-70e7cc44638c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
